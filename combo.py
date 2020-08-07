46a7cbde9a028abd19e58b7c6cef9f496b19f322
gerome12 <gerome.pasquier@gmail.com>
2020-08-07 18:36:05 +0200

    .

--- combo.py
+++ combo.py
@@ -6,9 +6,41 @@ from pynetdicom.sop_class import StudyRootQueryRetrieveInformationModelMove
 from dateutil.rrule import rrule, DAILY
 
 import datetime
+import time
 import sys
 import functools
 import os
+
+def get_c_find_association(ae_c_find, source_pacs_ip, source_pacs_port, ae_title):
+
+    for i in range(3):
+        assoc_c_find = ae_c_find.associate(source_pacs_ip, source_pacs_port, ae_title = source_pacs_ae_title)
+        if assoc_c_find.is_established:
+            return assoc_c_find
+        else:
+            if i != 2:
+                print(str(datetime.datetime.now()) +' Association (c_find) rejected, aborted or never connected (retry in 30 seconds)')
+                time.sleep(30)
+
+    print(str(datetime.datetime.now()) + ' Association (c_find) rejected, aborted or never connected')
+    sys.exit()
+
+
+
+def get_c_move_association(ae_c_move, source_pacs_ip, source_pacs_port, ae_title):
+
+    for i in range(3):
+        assoc_c_move = ae_c_move.associate(source_pacs_ip, source_pacs_port, ae_title = source_pacs_ae_title)
+        if assoc_c_move.is_established:
+            return assoc_c_move
+        else:
+            if i != 2:
+                print(str(datetime.datetime.now()) + ' Association (c_move) rejected, aborted or never connected (retry in 30 seconds)')
+                time.sleep(30)
+
+    print(str(datetime.datetime.now()) + ' Association (c_move) rejected, aborted or never connected')
+    sys.exit()
+  
 
 print = functools.partial(print, flush=True)
 
@@ -47,40 +79,40 @@ ds_c_find = Dataset()
 ds_c_find.QueryRetrieveLevel = 'STUDY'
 ds_c_find.StudyInstanceUID = ''
 
-# Associate with the peer AE
-assoc_c_find = ae_c_find.associate(source_pacs_ip, source_pacs_port, ae_title = source_pacs_ae_title)
-assoc_c_move = ae_c_move.associate(source_pacs_ip, source_pacs_port, ae_title = source_pacs_ae_title)
-if assoc_c_find.is_established and assoc_c_move.is_established:
-    for date in rrule(DAILY, dtstart=start_date, until=end_date):
 
-        f = open("logs/log"+date.strftime("%Y-%m")+".txt", "a")
+for date in rrule(DAILY, dtstart=start_date, until=end_date):
 
-        study_uid_lst = []
-        #C-FIND
-        ds_c_find.StudyDate =  date.strftime("%Y%m%d")
-        responses_c_find = assoc_c_find.send_c_find(ds_c_find, StudyRootQueryRetrieveInformationModelFind)
-        for (status_c_find, identifier_c_find) in responses_c_find:
-            if status_c_find.Status == 0xFF00:#Pending
-                print(date.strftime("%Y-%m-%d"))
-                print('\tstudyUID: ' + identifier_c_find.get('StudyInstanceUID'))
-                study_uid_lst.append(identifier_c_find.get('StudyInstanceUID'))
+    f = open("logs/log"+date.strftime("%Y-%m")+".txt", "a")
 
+    study_uid_lst = []
+    #C-FIND
+    ds_c_find.StudyDate =  date.strftime("%Y%m%d")
 
-        #C-MOVE
-        for study_uid in study_uid_lst:
+    assoc_c_find = get_c_find_association(ae_c_find, source_pacs_ip, source_pacs_port, source_pacs_ae_title)
+    responses_c_find = assoc_c_find.send_c_find(ds_c_find, StudyRootQueryRetrieveInformationModelFind)
+    for (status_c_find, identifier_c_find) in responses_c_find:
+        if status_c_find.Status == 0xFF00:#Pending
+            print(str(date.strftime("%Y-%m-%d")) + " (C-Find)")
+            print('\tstudyUID: ' + identifier_c_find.get('StudyInstanceUID'))
+            study_uid_lst.append(identifier_c_find.get('StudyInstanceUID'))
+    assoc_c_find.release()
 
-            ds_c_move = Dataset()
-            ds_c_move.QueryRetrieveLevel = 'STUDY'
-            ds_c_move.StudyInstanceUID = str(study_uid)
-            #try:
+    #C-MOVE
+    for study_uid in study_uid_lst:
+
+        ds_c_move = Dataset()
+        ds_c_move.QueryRetrieveLevel = 'STUDY'
+        ds_c_move.StudyInstanceUID = str(study_uid)
+        assoc_c_move = get_c_move_association(ae_c_move, source_pacs_ip, source_pacs_port, source_pacs_ae_title)
+        try:
             responses_c_move = assoc_c_move.send_c_move(ds_c_move, destination_pacs_ae_title, StudyRootQueryRetrieveInformationModelMove)
             for (status_c_move, identifier_c_move) in responses_c_move:
                 #status : Failure, Cancel, Warning, Success, Pending
                 if status_c_move:
                     if status_c_move.Status == 0xFF00:
-                        print('Pending')
+                        print('Pending (C-Move)')
                     elif status_c_move.Status == 0x0000:
-                        print('Success')
+                        print('Success (C-Move)')
                         print('Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value))
                         f.write(str(datetime.datetime.now()) + ' Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value) + ' ')
                         f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value) + ' ')
@@ -94,29 +126,26 @@ if assoc_c_find.is_established and assoc_c_move.is_established:
                             f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Success\r\n")
                         f.flush()
                     else:
-                        print('status : 0x{0:04X}'.format(status_c_move.Status))
+                        stringStatus = '0x{0:04X}'.format(status_c_move.Status)
+                        print('status : ' + stringStatus)
                         print(identifier_c_move)
                         print('Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)))
-                        f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error" + 'Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)) + "\r\n")
+                        f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error with status : " + stringStatus + ' Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)) + "\r\n")
                         f.flush()
                 else:
                     print('Connection timed out, was aborted or received invalid response')
                     f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error\r\n")
                     f.flush()
-            #except RuntimeError:
-            #    f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " RuntimeError\r\n")
-            #    f.flush() 
-            #except ValueError:
-            #    f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " ValueError\r\n")
-            #    f.flush()
+        except RuntimeError:
+            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " RuntimeError\r\n")
+            f.flush() 
+        except ValueError:
+            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " ValueError\r\n")
+            f.flush()
+        finally:
+            assoc_c_move.release()
 
-        f.close()
-
-    # Release the association
-    assoc_c_move.release()
-    assoc_c_find.release()
-else:
-    print('Association rejected, aborted or never connected')
+    f.close()
 
 f = open("logs/empty_months.txt", "a")
 for file in os.listdir("/logs"):
@@ -125,3 +154,5 @@ for file in os.listdir("/logs"):
         f.flush()
         os.remove("/logs/" + file)
 f.close()
+
+print('End of script')
