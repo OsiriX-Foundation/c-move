90855cd072d336f9c5469b97cb39d5654fe8c848
gerome12 <gerome.pasquier@gmail.com>
2020-08-07 16:53:21 +0200

    Update combo.py

--- combo.py
+++ combo.py
@@ -72,43 +72,43 @@ if assoc_c_find.is_established and assoc_c_move.is_established:
             ds_c_move = Dataset()
             ds_c_move.QueryRetrieveLevel = 'STUDY'
             ds_c_move.StudyInstanceUID = str(study_uid)
-            try:
-                responses_c_move = assoc_c_move.send_c_move(ds_c_move, destination_pacs_ae_title, StudyRootQueryRetrieveInformationModelMove)
-                for (status_c_move, identifier_c_move) in responses_c_move:
-                    #status : Failure, Cancel, Warning, Success, Pending
-                    if status_c_move:
-                        if status_c_move.Status == 0xFF00:
-                            print('Pending')
-                        elif status_c_move.Status == 0x0000:
-                            print('Success')
-                            print('Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value))
-                            f.write(str(datetime.datetime.now()) + ' Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value) + ' ')
-                            f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value) + ' ')
-                            f.write('Number of Warning Sub-operations ' + str(status_c_move.get(0x1023).value) + ' ')
-                            f.flush()
-                            if status_c_move.get(0x1022).value != 0 or status_c_move.get(0x1023).value != 0:
-                                f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " error")
-                                f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value))
-                                f.write('Number of Warning Sub-operations ' + str(status_c_move.get(0x1023).value) + "\r\n")
-                            else:
-                                f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Success\r\n")
-                            f.flush()
-                        else:
-                            print('status : 0x{0:04X}'.format(status_c_move.Status))
-                            print(identifier_c_move)
-                            print('Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)))
-                            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error" + 'Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)) + "\r\n")
-                            f.flush()
-                    else:
-                        print('Connection timed out, was aborted or received invalid response')
-                        f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error\r\n")
+            #try:
+            responses_c_move = assoc_c_move.send_c_move(ds_c_move, destination_pacs_ae_title, StudyRootQueryRetrieveInformationModelMove)
+            for (status_c_move, identifier_c_move) in responses_c_move:
+                #status : Failure, Cancel, Warning, Success, Pending
+                if status_c_move:
+                    if status_c_move.Status == 0xFF00:
+                        print('Pending')
+                    elif status_c_move.Status == 0x0000:
+                        print('Success')
+                        print('Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value))
+                        f.write(str(datetime.datetime.now()) + ' Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value) + ' ')
+                        f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value) + ' ')
+                        f.write('Number of Warning Sub-operations ' + str(status_c_move.get(0x1023).value) + ' ')
                         f.flush()
-            except RuntimeError:
-                f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " RuntimeError\r\n")
-                f.flush() 
-            except ValueError:
-                f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " ValueError\r\n")
-                f.flush()
+                        if status_c_move.get(0x1022).value != 0 or status_c_move.get(0x1023).value != 0:
+                            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " error")
+                            f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value))
+                            f.write('Number of Warning Sub-operations ' + str(status_c_move.get(0x1023).value) + "\r\n")
+                        else:
+                            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Success\r\n")
+                        f.flush()
+                    else:
+                        print('status : 0x{0:04X}'.format(status_c_move.Status))
+                        print(identifier_c_move)
+                        print('Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)))
+                        f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error" + 'Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)) + "\r\n")
+                        f.flush()
+                else:
+                    print('Connection timed out, was aborted or received invalid response')
+                    f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error\r\n")
+                    f.flush()
+            #except RuntimeError:
+            #    f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " RuntimeError\r\n")
+            #    f.flush() 
+            #except ValueError:
+            #    f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " ValueError\r\n")
+            #    f.flush()
 
         f.close()
 
