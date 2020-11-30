a951203996a273ae877d6d626c3cf5d2bb1a77c1
gerome12 <gerome.pasquier@gmail.com>
2020-11-30 13:42:42 +0100

    Update combo.py

--- combo.py
+++ combo.py
@@ -28,24 +28,19 @@ def get_c_find_association(ae_c_find, pacs_ip, pacs_port, ae_title):
 print = functools.partial(print, flush=True)
 
 
-if len(sys.argv) == 9:
-    s = str(sys.argv[1]).split("-")
-    start_date = datetime.datetime(int(s[0]), int(s[1]), int(s[2]))
-    s = str(sys.argv[2]).split("-")
-    end_date = datetime.datetime(int(s[0]), int(s[1]), int(s[2]))
-    source_pacs_ip = str(sys.argv[3])
-    source_pacs_port = int(sys.argv[4])
-    source_pacs_ae_title = str.encode(sys.argv[5])
-    destination_pacs_ip = str(sys.argv[6])
-    destination_pacs_port = int(sys.argv[7])
-    destination_pacs_ae_title = str.encode(sys.argv[8])
+if len(sys.argv) == 7:
+    source_pacs_ip = str(sys.argv[1])
+    source_pacs_port = int(sys.argv[2])
+    source_pacs_ae_title = str.encode(sys.argv[3])
+    destination_pacs_ip = str(sys.argv[4])
+    destination_pacs_port = int(sys.argv[5])
+    destination_pacs_ae_title = str.encode(sys.argv[6])
 else:
-    print("Usage : <start_date yyyy-mm-dd> <end_date yyyy-mm-dd> <source_pacs_ip> <source_pacs_port> <source_pacs_ae_title> <destination_pacs_ip> <destination_pacs_port> <destination_pacs_ae_title>")
+    print("Usage : <source_pacs_ip> <source_pacs_port> <source_pacs_ae_title> <destination_pacs_ip> <destination_pacs_port> <destination_pacs_ae_title>")
     exit(2)
 
 
 print("************************")
-print("Compare patientid study list from " + start_date.strftime("%Y-%m-%d") + " to " + end_date.strftime("%Y-%m-%d"))
 print("source pacs { ip :" + source_pacs_ip + " port :" + str(source_pacs_port) + " ae_title :" + str(source_pacs_ae_title) + "}")
 print("destination pacs { ip :" + destination_pacs_ip + " port :" + str(destination_pacs_port) + " ae_title :" + str(destination_pacs_ae_title) + "}")
 print("************************")
@@ -63,73 +58,66 @@ ae_c_find.add_requested_context(StudyRootQueryRetrieveInformationModelFind)
 # Create our Identifier (query) dataset
 ds_c_find = Dataset()
 ds_c_find.QueryRetrieveLevel = 'STUDY'
-ds_c_find.StudyInstanceUID = ''
 ds_c_find.PatientName = ''
 
 
-for date in rrule(DAILY, dtstart=start_date, until=end_date):
 
-    if date.strftime("%d") == "01": print(date.strftime("%Y-%m"))
+for filename in os.listdir("logspatientid"):
+    if filename != "empty_months.txt":
+        with open("logspatientid/"+filename) as f:
+            print(filename)
+            for line in f:
+                line = line.split()
 
-    f = open("logspatientid/log"+date.strftime("%Y-%m")+".txt", "a")
+                f = open("logspatientid2/"+filename, "a")
 
-    study_uid_lst = {}
-    #C-FIND
-    ds_c_find.StudyDate =  date.strftime("%Y%m%d")
+                study_uid_lst = {}
+                #C-FIND
+                ds_c_find.StudyInstanceUID = line[3]
 
-    try:
-        assoc_c_find = get_c_find_association(ae_c_find, destination_pacs_ip, destination_pacs_port, destination_pacs_ae_title)
-        responses_c_find = assoc_c_find.send_c_find(ds_c_find, StudyRootQueryRetrieveInformationModelFind)
-        for (status_c_find, identifier_c_find) in responses_c_find:
-            if status_c_find.Status == 0xFF00:#Pending
-                print("D:" + str(date.strftime("%Y-%m-%d")) + " (C-Find)")
-                study_uid_lst[identifier_c_find.get('StudyInstanceUID')] = identifier_c_find.get('PatientName') 
-    except RuntimeError:
-        print(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" c-find RuntimeError")
-        f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" c-find RuntimeError\r\n")
-        f.flush() 
-    except ValueError:
-        print(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" c-find ValueError")
-        f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" c-find ValueError\r\n")
-        f.flush()
-    finally:
-        assoc_c_find.release()
-  
-
-
-    for studyuid in study_uid_lst.keys():
-        ds_c_find1 = Dataset()
-        ds_c_find1.QueryRetrieveLevel = 'STUDY'
-        ds_c_find1.StudyInstanceUID = studyuid
-        ds_c_find1.PatientName = ''
-        try:
-            assoc_c_find = get_c_find_association(ae_c_find, source_pacs_ip, source_pacs_port, source_pacs_ae_title)
-            responses_c_find = assoc_c_find.send_c_find(ds_c_find1, StudyRootQueryRetrieveInformationModelFind)
-            for (status_c_find, identifier_c_find) in responses_c_find:
-                if status_c_find.Status == 0xFF00:#Pending
-                    print("S:" + str(date.strftime("%Y-%m-%d")) + " (C-Find)")
-                    if str(identifier_c_find.get('PatientName')) not in study_uid_lst[studyuid]:
-                        print("error")
-                        f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ studyuid + " ---" + str(identifier_c_find.get('PatientName')) + "---" + study_uid_lst[studyuid] +"\r\n")
-        except RuntimeError:
-            print(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" c-find RuntimeError")
-            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" c-find RuntimeError\r\n")
-            f.flush() 
-        except ValueError:
-            print(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" c-find ValueError")
-            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" c-find ValueError\r\n")
-            f.flush()
-        finally:
-            assoc_c_find.release()
-    f.close()
+                try:
+                    assoc_c_find = get_c_find_association(ae_c_find, destination_pacs_ip, destination_pacs_port, destination_pacs_ae_title)
+                    responses_c_find = assoc_c_find.send_c_find(ds_c_find, StudyRootQueryRetrieveInformationModelFind)
+                    for (status_c_find, identifier_c_find) in responses_c_find:
+                        if status_c_find.Status == 0xFF00:#Pending
+                            study_uid_lst[identifier_c_find.get('StudyInstanceUID')] = identifier_c_find.get('PatientName') 
+                except RuntimeError:
+                    print(str(datetime.datetime.now()) + " " + line[2] +" c-find RuntimeError")
+                    f.write(str(datetime.datetime.now()) + " " + line[2] +" c-find RuntimeError\r\n")
+                    f.flush() 
+                except ValueError:
+                    print(str(datetime.datetime.now()) + " " + line[2] +" c-find ValueError")
+                    f.write(str(datetime.datetime.now()) + " " + line[2] +" c-find ValueError\r\n")
+                    f.flush()
+                finally:
+                    assoc_c_find.release()
+            
 
 
-f = open("logspatientid/empty_months.txt", "a")
-for file in os.listdir("/logspatientid"):
-    if file.startswith("log") and file.endswith(".txt") and os.stat("/logspatientid/" + file).st_size == 0:
-        f.write(file.replace("log","").replace(".txt","") + "\r\n")
-        f.flush()
-        os.remove("/logspatientid/" + file)
-f.close()
+                for studyuid in study_uid_lst.keys():
+                    ds_c_find1 = Dataset()
+                    ds_c_find1.QueryRetrieveLevel = 'STUDY'
+                    ds_c_find1.StudyInstanceUID = studyuid
+                    ds_c_find1.PatientName = ''
+                    try:
+                        assoc_c_find = get_c_find_association(ae_c_find, source_pacs_ip, source_pacs_port, source_pacs_ae_title)
+                        responses_c_find = assoc_c_find.send_c_find(ds_c_find1, StudyRootQueryRetrieveInformationModelFind)
+                        for (status_c_find, identifier_c_find) in responses_c_find:
+                            if status_c_find.Status == 0xFF00:#Pending
+                                if str(identifier_c_find.get('PatientName')) not in study_uid_lst[studyuid]:
+                                    print("error")
+                                    f.write(str(datetime.datetime.now()) + " " + line[2] +" "+ studyuid + " ---" + str(identifier_c_find.get('PatientName')) + "---" + study_uid_lst[studyuid] +"\r\n")
+                    except RuntimeError:
+                        print(str(datetime.datetime.now()) + " " + line[2] +" c-find RuntimeError")
+                        f.write(str(datetime.datetime.now()) + " " + line[2] +" c-find RuntimeError\r\n")
+                        f.flush() 
+                    except ValueError:
+                        print(str(datetime.datetime.now()) + " " + line[2] +" c-find ValueError")
+                        f.write(str(datetime.datetime.now()) + " " + line[2] +" c-find ValueError\r\n")
+                        f.flush()
+                    finally:
+                        assoc_c_find.release()
+                f.close()
+
 
 print('End of script')
