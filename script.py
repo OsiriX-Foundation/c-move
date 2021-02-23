51f7c34557adaf362c5c4018ca5d73e7593119fc
gerome12 <gerome.pasquier@gmail.com>
2021-02-23 08:36:06 +0100

    Update script.py

--- script.py
+++ script.py
@@ -15,8 +15,8 @@ import requests
 AET = "PACS-JDWNRH"
 host_port = "pacs_arc_1:8080"
 
-logfile = open("logsdelete/log.txt", "a")
-logfilesuccess = open("logsdelete/success.txt", "a")
+logfile = open("logsdelete2/log.txt", "a")
+logfilesuccess = open("logsdelete2/success.txt", "a")
 
 
 arr = []
@@ -27,11 +27,22 @@ for filename in os.listdir("studylist"):
                 arr.append(line.split()[3])
 
 
-for studyUID in arr:
+"""for studyUID in arr:
 
     request_url = "http://"+host_port+"/dcm4chee-arc/aets/"+AET+"/rs/studies/"+studyUID+"/reject/113039%5EDCM"
     response = requests.post(request_url)
     if response.status_code != 200:
+        logfile.write(studyUID+" "+str(response.status_code)+"\n")
+        logfile.flush()
+    else:
+        logfilesuccess.write(studyUID+"\n")
+        logfilesuccess.flush()"""
+
+for studyUID in arr:
+
+    request_url = "http://"+host_port+"/dcm4chee-arc/aets/IOCM_EXPIRED/rs/studies/"+studyUID
+    response = requests.delete(request_url)
+    if response.status_code != 204:
         logfile.write(studyUID+" "+str(response.status_code)+"\n")
         logfile.flush()
     else:
@@ -41,11 +52,10 @@ for studyUID in arr:
 
 
 
-
-delete_url = "http://"+host_port+"/dcm4chee-arc/reject/113039%5EDCM?"
-response = requests.post(delete_url)
+"""delete_url = "http://"+host_port+"/dcm4chee-arc/reject/113039%5EDCM?"
+response = requests.delete(delete_url)
 if response.status_code != 204:
-    logfile.write("Last delete "+str(response.status_code))
+    logfile.write("Last delete "+str(response.status_code))"""
 
 logfile.close()
 logfilesuccess.close()
