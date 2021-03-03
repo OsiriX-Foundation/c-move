bd116a0fc8b667b34b6a5a8f2c7d62f03943616d
gerome12 <gerome.pasquier@gmail.com>
2021-03-03 16:30:53 +0100

    first commit

--- combo.py
+++ combo.py
@@ -2,8 +2,6 @@ from pydicom.dataset import Dataset
 from pynetdicom import AE, debug_logger
 from pynetdicom.sop_class import StudyRootQueryRetrieveInformationModelMove
 
-from dateutil.rrule import rrule, DAILY
-
 import datetime
 import time
 import sys
@@ -28,22 +26,17 @@ def get_c_move_association(ae_c_move, source_pacs_ip, source_pacs_port, ae_title
 print = functools.partial(print, flush=True)
 
 
-if len(sys.argv) == 7:
-    s = str(sys.argv[1]).split("-")
-    start_date = datetime.datetime(int(s[0]), int(s[1]), int(s[2]))
-    s = str(sys.argv[2]).split("-")
-    end_date = datetime.datetime(int(s[0]), int(s[1]), int(s[2]))
-    source_pacs_ip = str(sys.argv[3])
-    source_pacs_port = int(sys.argv[4])
-    source_pacs_ae_title = str.encode(sys.argv[5])
-    destination_pacs_ae_title = str.encode(sys.argv[6])
+if len(sys.argv) == 5:
+    source_pacs_ip = str(sys.argv[1])
+    source_pacs_port = int(sys.argv[2])
+    source_pacs_ae_title = str.encode(sys.argv[3])
+    destination_pacs_ae_title = str.encode(sys.argv[4])
 else:
-    print("Usage : <start_date yyyy-mm-dd> <end_date yyyy-mm-dd> <source_pacs_ip> <source_pacs_port> <source_pacs_ae_title> <destination_pacs_ae_title>")
+    print("Usage : <source_pacs_ip> <source_pacs_port> <source_pacs_ae_title> <destination_pacs_ae_title>")
     exit(2)
 
 
 print("************************")
-print("C-MOVE from " + start_date.strftime("%Y-%m-%d") + " to " + end_date.strftime("%Y-%m-%d"))
 print("source pacs { ip :" + source_pacs_ip + " port :" + str(source_pacs_port) + " ae_title :" + str(source_pacs_ae_title) + "}")
 print("destination pacs { ae_title :" + str(destination_pacs_ae_title) + "}")
 print("************************")
@@ -55,96 +48,76 @@ ae_c_move = AE()
 ae_c_move.add_requested_context(StudyRootQueryRetrieveInformationModelMove)
 
 
-arr = []
-for filename in os.listdir("logs2"):
-    if filename != "empty_months.txt":
-        with open("logs2/"+filename) as f:
-            for line in f:
-                if "Error" in line:
-                    arr.append(line.split()[2] + " " +line.split()[3])
-
-arr = list(dict.fromkeys(arr))
-arr2 = {}
-for a in arr:
-    if a.split()[0] in arr2.keys():
-        arr2[a.split()[0]].append(a.split()[1])
-    else:
-        arr2[a.split()[0]] = [a.split()[1]]
+study_uid_lst = []
+for filename in os.listdir("logspatientid2"):
+    with open("logspatientid2/"+filename) as f:
+        for line in f:
+            study_uid_lst.append(line.split()[3])
 
 
-for date in rrule(DAILY, dtstart=start_date, until=end_date):
+f = open("logtransfert/log.txt", "a")
+f2 = open("logtransfert/success.txt", "a")
 
-    f = open("logs3/log"+date.strftime("%Y-%m")+".txt", "a")
+    
 
-        
-    study_uid_lst = []
-    if date.strftime("%Y-%m-%d") in arr2.keys():
-        study_uid_lst = arr2[date.strftime("%Y-%m-%d")]
-    else:
-        study_uid_lst = []
+#C-MOVE
+for study_uid in study_uid_lst:
 
-
-    #C-MOVE
-    for study_uid in study_uid_lst:
-
-        ds_c_move = Dataset()
-        ds_c_move.QueryRetrieveLevel = 'STUDY'
-        ds_c_move.StudyInstanceUID = str(study_uid)
-        assoc_c_move = get_c_move_association(ae_c_move, source_pacs_ip, source_pacs_port, source_pacs_ae_title)
-        try:
-            responses_c_move = assoc_c_move.send_c_move(ds_c_move, destination_pacs_ae_title, StudyRootQueryRetrieveInformationModelMove)
-            for (status_c_move, identifier_c_move) in responses_c_move:
-                #status : Failure, Cancel, Warning, Success, Pending
-                if status_c_move:
-                    if status_c_move.Status == 0xFF00:
-                        print('Pending (C-Move)')
-                    elif status_c_move.Status == 0x0000:
-                        print('Success (C-Move)')
-                        print('Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value))
-                        f.write(str(datetime.datetime.now()) + ' Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value) + ' ')
-                        f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value) + ' ')
-                        f.write('Number of Warning Sub-operations ' + str(status_c_move.get(0x1023).value) + ' ')
-                        f.flush()
-                        if status_c_move.get(0x1022).value != 0 or status_c_move.get(0x1023).value != 0:
-                            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " error")
-                            f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value))
-                            f.write('Number of Warning Sub-operations ' + str(status_c_move.get(0x1023).value) + "\r\n")
-                        else:
-                            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Success\r\n")
+    ds_c_move = Dataset()
+    ds_c_move.QueryRetrieveLevel = 'STUDY'
+    ds_c_move.StudyInstanceUID = str(study_uid)
+    assoc_c_move = get_c_move_association(ae_c_move, source_pacs_ip, source_pacs_port, source_pacs_ae_title)
+    try:
+        responses_c_move = assoc_c_move.send_c_move(ds_c_move, destination_pacs_ae_title, StudyRootQueryRetrieveInformationModelMove)
+        for (status_c_move, identifier_c_move) in responses_c_move:
+            #status : Failure, Cancel, Warning, Success, Pending
+            if status_c_move:
+                if status_c_move.Status == 0xFF00:
+                    print('Pending (C-Move)')
+                elif status_c_move.Status == 0x0000:
+                    print('Success (C-Move)')
+                    print('Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value))
+                    f.write(str(datetime.datetime.now()) + ' Number of Completed Sub-operations ' + str(status_c_move.get(0x1021).value) + ' ')
+                    f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value) + ' ')
+                    f.write('Number of Warning Sub-operations ' + str(status_c_move.get(0x1023).value) + ' ')
+                    f.flush()
+                    if status_c_move.get(0x1022).value != 0 or status_c_move.get(0x1023).value != 0:
+                        f.write(str(datetime.datetime.now()) +" "+ study_uid + " error")
+                        f.write('Number of Failed Sub-operations ' + str(status_c_move.get(0x1022).value))
+                        f.write('Number of Warning Sub-operations ' + str(status_c_move.get(0x1023).value) + "\r\n")
                         f.flush()
                     else:
-                        stringStatus = '0x{0:04X}'.format(status_c_move.Status)
-                        print('status : ' + stringStatus)
-                        print(identifier_c_move)
-                        if identifier_c_move is not None:
-                            print('Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)))
-                            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error with status : " + stringStatus + ' Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)) + "\r\n")
-                        else:
-                            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error with status : " + stringStatus + ' Failed SOP Instance UID List ' + "\r\n")
+                        f.write(str(datetime.datetime.now()) + study_uid + " Success\r\n")
                         f.flush()
+                        f2.write(str(study_uid) + '\r\n')
+                        f2.flush()
                 else:
-                    print('Connection timed out, was aborted or received invalid response')
-                    f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " Error\r\n")
+                    stringStatus = '0x{0:04X}'.format(status_c_move.Status)
+                    print('status : ' + stringStatus)
+                    print(identifier_c_move)
+                    if identifier_c_move is not None:
+                        print('Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)))
+                        f.write(str(datetime.datetime.now()) +" "+ study_uid + " Error with status : " + stringStatus + ' Failed SOP Instance UID List ' + str(identifier_c_move.get(0x00080058)) + "\r\n")
+                    else:
+                        f.write(str(datetime.datetime.now()) +" "+ study_uid + " Error with status : " + stringStatus + ' Failed SOP Instance UID List ' + "\r\n")
                     f.flush()
-        except RuntimeError:
-            print(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " RuntimeError")
-            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " RuntimeError\r\n")
-            f.flush() 
-        except ValueError:
-            print(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " ValueError")
-            f.write(str(datetime.datetime.now()) + " " + date.strftime("%Y-%m-%d") +" "+ study_uid + " ValueError\r\n")
-            f.flush()
-        finally:
-            assoc_c_move.release()
-
-    f.close()
-
-f = open("logs3/empty_months.txt", "a")
-for file in os.listdir("/logs3"):
-    if file.startswith("log") and file.endswith(".txt") and os.stat("/logs3/" + file).st_size == 0:
-        f.write(file.replace("log","").replace(".txt","") + "\r\n")
+            else:
+                print('Connection timed out, was aborted or received invalid response')
+                f.write(str(datetime.datetime.now()) + " " + study_uid + " Error\r\n")
+                f.flush()
+    except RuntimeError:
+        print(str(datetime.datetime.now()) + " " + study_uid + " RuntimeError")
+        f.write(str(datetime.datetime.now()) + " " + study_uid + " RuntimeError\r\n")
+        f.flush() 
+    except ValueError:
+        print(str(datetime.datetime.now()) + " " + study_uid + " ValueError")
+        f.write(str(datetime.datetime.now()) + " " + study_uid + " ValueError\r\n")
         f.flush()
-        os.remove("/logs3/" + file)
+    finally:
+        assoc_c_move.release()
+
 f.close()
+f2.close()
+
 
 print('End of script')
